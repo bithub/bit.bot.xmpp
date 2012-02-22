71fac75a5f7af4509167af32a3ceb00d5ecc1553
Ryan Northey <ryan@3ca.org.uk>
2012-02-22 23:07:00 +0000

    zcml

--- bit/bot/xmpp/plugin.py
+++ bit/bot/xmpp/plugin.py
@@ -12,26 +12,3 @@ from bit.core.interfaces import IPlugin, IConfiguration, IServices
 from bit.bot.common.interfaces import IJabber
 from bit.bot.base.plugin import BotPlugin
 
-from bit.bot.xmpp.presence import BotPresence
-from bit.bot.xmpp.bot import BotProtocol
-
-class BotXMPP(BotPlugin):
-    implements(IPlugin)
-    name = 'bit.bot.xmpp'
-
-    @property
-    def utils(self):
-        configuration = getUtility(IConfiguration)        
-        bot_jid = JID(configuration.get('bot','jid'))
-        password = configuration.get('passwords',configuration.get('bot','password'))
-        return [(client.XMPPClient(bot_jid, password),IJabber)]
-        
-    def load_services(self):        
-        bot = getUtility(IJabber)
-        curatebot = BotProtocol()        
-        presence = BotPresence(curatebot)
-        curatebot._presence = presence
-        curatebot.setHandlerParent(bot)    
-        presence.setHandlerParent(bot)
-        presence.available()
-        getUtility(IServices).add('bit.bot.xmpp', {'bot': bot})
---
+++ bit/bot/xmpp/xmpp.py
@@ -0,0 +1,30 @@
+
+
+from zope.interface import implementer
+from zope.component import getUtility
+
+from twisted.words.protocols.jabber.jid import JID
+
+from bit.core.interfaces import IConfiguration
+from bit.bot.common.interfaces import IJabber
+from bit.bot.xmpp.presence import BotPresence
+from bit.bot.xmpp.bot import BotProtocol
+
+from wokkel import client
+
+@implementer(IJabber)
+def botXMPP():
+    configuration = getUtility(IConfiguration)        
+    bot_jid = JID(configuration.get('bot','jid'))
+    password = configuration.get('passwords',configuration.get('bot','password'))
+    return client.XMPPClient(bot_jid, password)
+
+def botFactory():
+    bot = getUtility(IJabber)
+    curatebot = BotProtocol()        
+    presence = BotPresence(curatebot)
+    curatebot._presence = presence
+    curatebot.setHandlerParent(bot)    
+    presence.setHandlerParent(bot)
+    presence.available()
+    return bot
