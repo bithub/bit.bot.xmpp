e03eaec802664959bee1f2f4d3a964012861af6e
Ryan Northey <ryan@3ca.org.uk>
2012-03-24 15:33:22 +0000

    added xmpp zcml

---
+++ bit/bot/xmpp/zcml.py
@@ -0,0 +1,40 @@
+import os
+import zope
+import bit
+from StringIO import StringIO
+
+from zope.configuration.xmlconfig import xmlconfig
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory('bit.bot.xmpp')
+
+
+class IXMPPDirective(zope.interface.Interface):
+    """
+    Define a xmpp
+    """
+
+    name = zope.schema.TextLine(
+        title=_("Name"),
+        description=_("The xmpp name"),
+        required=True,
+        )
+    parent = zope.schema.TextLine(
+        title=_("Name"),
+        description=_("The service parent"),
+        required=True,
+        )
+    factory = zope.configuration.fields.GlobalObject(
+        title=_("XMPP factory"),
+        description=_("The xmpp factory"),
+        required=True,
+        )
+
+def xmpp(_context, parent, name, factory):
+
+    services = zope.component.getUtility(bit.core.interfaces.IServices)
+    _xmpps = {name: factory()}
+    _context.action(
+        discriminator=None,
+        callable=services.add,
+        args=(parent, _xmpps)
+        )
